c8ddbc174d2be05023f16127c71686e65af0b71d
vivek22117 <vivekmishra22117@gmail.com>
2021-12-09 21:47:33 +0530

    updating data tf file to read state file for lambda fixed resoruces

--- aws-tf-modules/lambda-resources/data.tf
+++ aws-tf-modules/lambda-resources/data.tf
@@ -6,7 +6,7 @@ data "terraform_remote_state" "s3_buckets" {
   backend = "s3"
 
   config = {
-    bucket  = "${var.s3_bucket_prefix}-${var.environment}-${var.default_region}"
+    bucket  = "${var.environment}-${var.s3_bucket_prefix}-${data.aws_caller_identity.current.account_id}-${var.default_region}"
     key     = "state/${var.environment}/s3-buckets/terraform.tfstate"
     region  = var.default_region
   }
@@ -16,8 +16,10 @@ data "terraform_remote_state" "lambda_fixed_resources" {
   backend = "s3"
 
   config = {
-    bucket  = "${var.s3_bucket_prefix}-${var.environment}-${var.default_region}"
+    bucket  = "${var.environment}-${var.s3_bucket_prefix}-${data.aws_caller_identity.current.account_id}-${var.default_region}"
     key     = "state/${var.environment}/lambda/rsvp-lambda-fixed-resources/terraform.tfstate"
     region  = var.default_region
   }
 }
+
+data "aws_caller_identity" "current" {}
